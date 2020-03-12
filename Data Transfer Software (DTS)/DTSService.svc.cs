ebf95b6bd8400c13ae3cf0e0f5b015943f25fcb4
Darren Oliveiro-Priestnall <darren.op@atlascity.io>
2020-03-12 16:21:46 +0000

    first commit

---
+++ Data Transfer Software (DTS)/DTSService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using RCCFLib;
+// using RCCFLib.Records;
+
+namespace Data_Transfer_Software__DTS_
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DTSService" in code, svc and config file together.
+    public class DTSService : IDTSService
+    {
+        public List<RCCFLib.Records.RecordingSet> GetRecordingSetSeries(int recordingSetId)
+        {
+            return DataAccess.GetRecordingSetSeries(recordingSetId);
+        }
+
+        public List<RCCFLib.Records.RecordingSet> GetRecordingSetSeriesFrom(int recordingSetId, DateTime fromDateTime)
+        {
+            return DataAccess.GetRecordingSetSeriesFrom(recordingSetId, fromDateTime);
+        }
+
+        public List<RCCFLib.Records.RecordingSet> GetRecordingSetSeriesFromTo(int recordingSetId, DateTime fromDateTime, DateTime toDateTime)
+        {
+            return DataAccess.GetRecordingSetSeriesFromTo(recordingSetId, fromDateTime, toDateTime);
+        }
+
+        public RCCFLib.Records.RecordingSet GetLatestReading(int recordingSetId)
+        {
+            return DataAccess.GetLatestReading(recordingSetId);
+        }
+
+        public void AddRecordingSet(RCCFLib.Records.RecordingSet recordingSet)
+        {
+            DataAccess.AddRecordingSet(recordingSet);
+        }
+    }
+}
---
+++ Data Transfer Software (DTS)/IDTSService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using RCCFLib;
+
+namespace Data_Transfer_Software__DTS_
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDTSService" in both code and config file together.
+    [ServiceContract]
+    public interface IDTSService
+    {
+        [OperationContract]
+        List<RCCFLib.Records.RecordingSet> GetRecordingSetSeries(int recordingSetId);
+
+        [OperationContract]
+        List<RCCFLib.Records.RecordingSet> GetRecordingSetSeriesFrom(int recordingSetId, DateTime fromDateTime);
+
+        [OperationContract]
+        List<RCCFLib.Records.RecordingSet> GetRecordingSetSeriesFromTo(int recordingSetId, DateTime fromDateTime, DateTime toDateTime);
+
+        [OperationContract]
+        RCCFLib.Records.RecordingSet GetLatestReading(int recordingSetId);
+
+        [OperationContract]
+        void AddRecordingSet(RCCFLib.Records.RecordingSet recordingSet);
+    }
+}
